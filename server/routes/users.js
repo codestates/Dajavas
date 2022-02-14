51250a0fbd57387a521aa901989725932fe6784c
gardenia1109 <gardenia1109@naver.com>
2022-02-14 16:11:26 +0900

    FIX: Routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const controllers = require("../controllers/user/user");
+const controllers = require("../controllers/user");
 
 /* GET users listing. */
-router.get("/", controllers.get);
-router.post("/", controllers.post);
-router.put("/", controllers.put);
-router.delete("/", controllers.delete);
+router.get("/mypage", controllers.mypage.get);
+router.get("/emailcheck", controllers.emailcheck.get);
+router.get("/namecheck", controllers.namecheck.get);
+router.post("/login", controllers.login.post);
+router.post("/signup", controllers.signup.post);
+router.post("/logout", controllers.logout.post);
+router.put("/mypage", controllers.mypage.put);
+router.delete("/mypage", controllers.mypage.delete);
 
 module.exports = router;
