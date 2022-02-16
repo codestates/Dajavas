43844b8fec6c5e50e75384e60ad7005346c13d32
Jhin3283 <hj3283@gmail.com>
2022-02-16 11:22:57 +0900

    FIX: fish,map,ranking

--- server/controllers/ranking/ranking.js
+++ server/controllers/ranking/ranking.js
@@ -8,27 +8,46 @@ module.exports = {
     const page = req.query.page || 1;
     const result = [];
     const filteredFish = await models.fish.findAll({
-      where: { fish_name: fishName },
+      where: { ranked: 1, fish_name: fishName },
     });
-    function arrPush() {
+    async function arrPush() {
+      // for (let i = 0; i < filteredFish.length; i++) {
+      //   allFiltered.push(
+      //     await models.fish.findOne({
+      //       where: { fish_name: fishName,  },
+      //     })[i]
+      //   );
+      // }
+      // allFiltered.sort(function (a, b) {
+      //   if (a.size > b.size) return -1;
+      //   if (a.size === b.size) return 0;
+      //   if (a.size < b.size) return 1;
+      // });
+      // for (let i = 0; i < 5 * page; i++) {
+      //   result.push(allFiltered[i]);
+      // }
+      // console.log(result);
       for (let i = 0; i < filteredFish.length; i++) {
-        allFiltered.push(
-          await models.fish.findAll({
-            where: { fish_name: fishName },
-          })[i]
-        );
+        const findRank = await models.fish.findOne({
+          where: { fish_name: filteredFish[i].fish_name },
+        });
+        result.push({
+          fish_name: findRank.fish_name,
+          src: findRank.src,
+          size: findRank.size,
+          ranked: findRank.ranked,
+          createdAt: findRank.createdAt,
+        });
       }
-      allFiltered.sort(function (a, b) {
+      result.sort(function (a, b) {
         if (a.size > b.size) return -1;
         if (a.size === b.size) return 0;
         if (a.size < b.size) return 1;
       });
-      for (let i = 0; i < 5 * page; i++) {
-        result.push(allFiltered[i]);
-      }
     }
     try {
-      arrPush();
+      console.log(filteredFish);
+      await arrPush();
       return res.status(200).send({ data: { result } });
     } catch {
       return console.log("품종별 크기 랭킹 조회 잘못되었음");
