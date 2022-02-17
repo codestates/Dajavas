3812b2ed15f2577dfce063159057feb79a7d0a24
gardenia1109 <gardenia1109@naver.com>
2022-02-17 17:31:53 +0900

    ADD: Kakao

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,44 +1,34 @@
-import React from 'react'
-import Sidebar from '../Sidebar/Sidebar';
+import React from "react";
+import Sidebar from "../Sidebar/Sidebar";
+import Kakao from "./kakao";
 
 function Login() {
+  const moveToHome = () => {
+    window.location.replace("/");
+  };
+  const moveToSignup = () => {
+    window.location.href = "/signup";
+  };
 
-    const moveToHome = () => {
-        window.location.replace('/');
-    }
-    const moveToSignup = () => {
-        window.location.href='/signup';
-    }
-
-    return (
-        <>
-            <div>
-                login
-                <div>
-                    <input type='text' placeholder='아이디' />
-                </div>
-                <div>
-                    <input type='text' placeholder='비밀번호' />
-                </div>
-                <button onClick={moveToHome}>로그인</button>
-            </div>
-            <button>
-                구글로 로그인
-            </button>
-            <button>
-                카카오로 로그인
-            </button>
-            <button onClick={moveToHome}>
-                홈으로
-            </button>
-            <div>
-                아직 아이디가 없으신가요?
-            </div>
-            <button onClick={moveToSignup}>
-                회원가입
-            </button>
-        </>
-    )
+  return (
+    <>
+      <div>
+        login
+        <div>
+          <input type="text" placeholder="아이디" />
+        </div>
+        <div>
+          <input type="text" placeholder="비밀번호" />
+        </div>
+        <button onClick={moveToHome}>로그인</button>
+      </div>
+      <button>구글로 로그인</button>
+      <Kakao />
+      <button onClick={moveToHome}>홈으로</button>
+      <div>아직 아이디가 없으신가요?</div>
+      <button onClick={moveToSignup}>회원가입</button>
+    </>
+  );
 }
 
-export default Login
+export default Login;
---
+++ client/src/components/Login/kakao.js
@@ -0,0 +1,35 @@
+import { React } from "react";
+import axios from "axios";
+import qs from "qs";
+import { KAKAO_URL, REST_API_KEY, SECRET_KEY, REDIRECT_URI } from "./oauth";
+
+function kakao() {
+  // oauth kakao login
+  // 코드 값 받아오기!
+  let code = new URL(window.location.href).searchParams.get("code");
+
+  const kakao = async () => {
+    try {
+      const result = await axios.get(
+        `${process.env.REDIRECT_URI}/api/v1/oauth2/authorization/kakao?code=${code}`
+      );
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+  return (
+    <>
+      <div>
+        <a href={KAKAO_URL}>
+          <img
+            src="//k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg"
+            width="222"
+            alt="카카오 로그인"
+          />
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default kakao;
