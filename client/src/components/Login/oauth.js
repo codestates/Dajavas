c02c55e4fd775b9409ad97d12f18ccb50e985148
gardenia1109 <gardenia1109@naver.com>
2022-02-17 17:33:08 +0900

    MERGE

---
+++ client/src/components/Login/oauth.js
@@ -0,0 +1,6 @@
+// 숨기는 키들 *
+
+export const REST_API_KEY = "90bcddbd3639f8cd5b053485c8ac8a8c";
+export const REDIRECT_URI = "https://localhost:3000/";
+export const SECRET_KEY = "gfU7mXSQJueiiIMoeOTqNQoWWVwssLJQ";
+export const KAKAO_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
