e31e9aa111aeddaa1480b5f28576ca19ef7710d6
Boyoung <boyoung.y589@gmail.com>
2022-02-22 21:48:03 +0900

    Fixed: Redux

--- client/src/store/actions/actionTypes.js
+++
@@ -1,8 +0,0 @@
-//user
-export const LOG_IN = 'LOG_IN';
-export const LOG_OUT = 'LOG_OUT';
-export const UPDATE_INFO = 'UPDATE_INFO';
-
-//Modal
-export const CONFIRM_MODAL_ON = 'CONFIRM_MODAL_ON';
-export const MODAL_OFF = 'MODAL_OFF';
