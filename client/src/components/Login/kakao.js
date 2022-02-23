e42cd6ff4f5e39b81c8aaaf3969ef0bb7cbba187
gardenia1109 <gardenia1109@naver.com>
2022-02-23 10:41:49 +0900

    ADD:Kakao

--- client/src/components/Login/kakao.js
+++ client/src/components/Login/kakao.js
@@ -1,36 +1,72 @@
-import { React } from "react";
 import axios from "axios";
-import qs from "qs";
-import { KAKAO_URL, REST_API_KEY, SECRET_KEY, REDIRECT_URI } from "./oauth";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { REST_API_KEY, REDIRECT_URI } from "./oauth";
 
-function kakao() {
-  // oauth kakao login
-  // 코드 값 받아오기!
-  let code = new URL(window.location.href).searchParams.get("code");
+const Kakao = ({ LoginHanlder }) => {
+  const navigate = useNavigate();
+  const code = new URL(window.location.href).searchParams.get("code");
+  console.log("code : ", code);
+  const makeFormData = (params) => {
+    const searchParams = new URLSearchParams();
+    Object.keys(params).forEach((key) => {
+      searchParams.append(key, params[key]);
+    });
 
-  const kakao = async () => {
-    try {
-      //
-      const result = await axios.get(
-        `${process.env.REDIRECT_URI}/api/v1/oauth2/authorization/kakao?code=${code}`
-      );
-    } catch (error) {
-      console.log("error", error);
-    }
+    return searchParams;
   };
-  return (
-    <>
-      <div>
-        <a href={KAKAO_URL}>
-          <img
-            src="//k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg"
-            width="222"
-            alt="카카오 로그인"
-          />
-        </a>
-      </div>
-    </>
-  );
-}
 
-export default kakao;
+  axios({
+    method: "POST",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded;charset=utf-8",
+    },
+    url: "https://kauth.kakao.com/oauth/token",
+    data: makeFormData({
+      grant_type: "authorization_code",
+      client_id: REST_API_KEY,
+      redirect_uri: REDIRECT_URI,
+      code,
+    }),
+  }).then((res) => {
+    console.log("res : ", res);
+    console.log("res.data : ", res.data);
+    const ACCESS_TOKEN = res.data.access_token;
+    console.log(ACCESS_TOKEN);
+    kakaoTokenToServer(ACCESS_TOKEN);
+  });
+
+  const kakaoTokenToServer = (token) => {
+    console.log("여기로오나?");
+    // console.log(token);
+    axios
+      .post("http://localhost:4000/oauth/kakao", { access_token: token })
+      .then((res) => {
+        if (res.status === 201 || res.status === 200) {
+          const user = res.data;
+          console.log("user : ", user);
+          // window.localStorage.setItem(
+          //   "token",
+          //   JSON.stringify({
+          //     access_token: res.data.jwt,
+          //   })
+          // );
+          // user.data.id,
+          // user.data.properties.nickname
+          // user.data.kakao_account.email
+          const userInfo = {
+            id: user.data.id,
+            nickname: user.data.properties.nickname,
+            email: user.data.kakao_account.email,
+          };
+          LoginHanlder(userInfo);
+          navigate("/");
+        } else {
+          window.alert("로그인에 실패하였습니다.");
+        }
+      });
+  };
+  return <></>;
+};
+
+export default Kakao;
